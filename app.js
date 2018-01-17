87c416b073728caa71f3e32069db039cfa187bec
juanmi <juanmiguel@existo.es>
2018-01-17 10:02:53 +0100

    Creación plantilla ejemplo con bucle

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var nunjucks = require('nunjucks');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -17,9 +14,9 @@ app.set('views', path.join(__dirname, 'views'));
 
 nunjucks.configure('views', {
   autoescape: true,
+  noCache: true,
   express   : app
 });
-
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -29,8 +26,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', require('./routes/index'));
+app.use('/item/:id', require('./routes/item'));
+app.use('/users', require('./routes/users'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/item.js
+++ routes/item.js
@@ -3,9 +3,17 @@ var router = express.Router();
 
 var items = require('../models/items');
 
+const item = { item: {
+				id: "1",
+				nombre: "Gris",
+				descripcion: "Lorem	ipsum para item 1",
+				color: "ccc"
+			}
+		}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {items: items});
+  res.render('item', item);
 });
 
 module.exports = router;
