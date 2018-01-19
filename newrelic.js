ec29a3bbc26211f14bb3743ea3a4bf0fa5035ccf
juanmi <juanmiguel@existo.es>
2018-01-19 08:50:42 +0100

    Configuración New Relic

---
+++ newrelic.js
@@ -0,0 +1,25 @@
+'use strict'
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config/default.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['test-base-scaffold'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: 'a7706fff7622ba64b3d6de1eed594e5f3caab31d',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
